360a6375b3b07fa2a293e3ff61aa64420a61b518
franmatijevic <64538049+franmatijevic@users.noreply.github.com>
2022-06-16 14:27:25 +0200

    Jos malih promjena

--- Rogue Platformer/src/environment/bedrock.gd
+++ Rogue Platformer/src/environment/bedrock.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 
 func _ready() -> void:
+	if(get_node("/root/Game/World").has_node("Beholder")):
+		get_node("CollisionShape2D").disabled=false
 	yield(get_tree().create_timer(1), "timeout")
 	get_node("CollisionShape2D").disabled=false
 
